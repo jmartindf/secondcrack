16e75d1b0fcab25e77633f0d4b27fe185d606298
Joe Martin <joe@desertflood.com>
2013-03-15 13:10:34 -0500

    Update standalone script to support multiple hubs and multiple feeds

--- engine/pubsubhubbub.php
+++ engine/pubsubhubbub.php
@@ -1,13 +1,15 @@
 <?php
 include("publisher.php");
 
-$hub_url = "https://pubsubhubbub.appspot.com/publish";
-$feed_urls = array("http://minorthoughts.com/rss.xml");
-$p = new Publisher($hub_url);
-if($p->publish_update($feed_urls)) {
-  error_log('Published via PubSubHubbub.');
-} else {
-  error_log('Could not publish via PubSubHubbub.');
+$hub_urls = array("http://minorthoughts.superfeedr.com/","http://pubsubhubbub.appspot.com/publish");
+$feed_urls = array("http://minorthoughts.com/rss.xml","http://minorthoughts.com/rss-excerpts.xml");
+foreach ($hub_urls as $hub_url) {
+  $p = new Publisher($hub_url);
+  if($p->publish_update($feed_urls)) {
+    error_log('Published via PubSubHubbub.');
+  } else {
+    error_log('Could not publish via PubSubHubbub.');
+  }
 }
 
 ?>
